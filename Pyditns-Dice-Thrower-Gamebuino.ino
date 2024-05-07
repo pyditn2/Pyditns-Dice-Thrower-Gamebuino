34f2997670cffb8f4a82cbcd69d6433c16471b77
benwh <ben.herrmann00@gmx.de>
2024-05-08 00:10:02 +0200

    adder allignment

--- Pyditns-Dice-Thrower-Gamebuino.ino
+++ Pyditns-Dice-Thrower-Gamebuino.ino
@@ -81,6 +81,7 @@ void loop(){
     }
   }
   showDisplay();
+  showAdder();
 }
 
 void initGame(){
--- adder.ino
+++ adder.ino
@@ -3,4 +3,11 @@ Adder adder;
 
 void initAdder(){
   adder.total = 0;
+  
+}
+
+void showAdder(){
+  gb.display.cursorX = 0;
+  gb.display.cursorY = 42;
+  gb.display.print("Adder working");
 }
